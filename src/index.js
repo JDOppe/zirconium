ac7b94d57ef520d2d32fc7b309979842a6ae08cd
Jeffrey Oppe <jeffreyoppe@gmail.com>
2025-04-16 12:06:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,76 +16,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const pages = [
-  { url: "/", lastmod: "2024-01-01", priority: "1.0" },
-];
-
-app.get("/sitemap.xml", (req, res) => {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map(
-        (page) => `
-      <url>
-        <loc>https://arsenic.smartfoloo.space${page.url}</loc>
-        <lastmod>${page.lastmod}</lastmod>
-        <priority>${page.priority}</priority>
-      </url>`
-      )
-      .join("")}
-  </urlset>`;
-
-  res.header("Content-Type", "application/xml");
-  res.send(sitemap);
-});
-
-const server = createServer();
-
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else if (req.url.endsWith('/wisp/')) {
-    wisp.routeRequest(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-
-// The standalone server needs to start ONLY if not running on Vercel
-if (!process.env.VERCEL) {
-  let port = parseInt(process.env.PORT, 10);
-  if (isNaN(port)) port = 8080;
-
-  server.on("listening", () => {
-    const address = server.address();
-    console.log("Listening on:");
-    console.log(`\thttp://localhost:${address.port}`);
-    console.log(`\thttp://${hostname()}:${address.port}`);
-    console.log(`\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address}:${address.port}`);
-  });
-
-  // Graceful shutdown
-  process.on("SIGINT", shutdown);
-  process.on("SIGTERM", shutdown);
-
-  function shutdown() {
-    console.log("SIGTERM signal received: closing HTTP server");
-    server.close();
-    bare.close();
-    process.exit(0);
-  }
-
-  server.listen({ port });
-}
-
+// Export the Express app as the Vercel handler
 module.exports = (req, res) => {
   if (bare.shouldRoute(req)) {
     bare.routeRequest(req, res);
