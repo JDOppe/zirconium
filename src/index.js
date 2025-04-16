7f775419ea2ab775d3d7a8aaacd17574877edb8c
Jeffrey Oppe <jeffreyoppe@gmail.com>
2025-04-16 08:49:33 -0500

    Attempt to correct "Cannot GET /" #3

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use("/uv/", express.static(uvPath));
 app.use('/baremux/', express.static(baremuxPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join('./public', 'index.html'));
 });
 
 const pages = [
