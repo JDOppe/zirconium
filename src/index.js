519f9cc8da853824dc1e2ae723b106db8ceaaa3a
Jeffrey Oppe <jeffreyoppe@gmail.com>
2025-04-16 12:10:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const bare = createBareServer("/bare/");
 const app = express();
 
 // Serve static files from the 'public' directory at the root
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.cwd(), 'public')));
 app.use("/uv/", express.static(uvPath));
 app.use('/baremux/', express.static(baremuxPath));
 
 // Explicitly handle the root path to serve index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
 // Handle all other requests by passing them to the Express app
